b592291cc24fe884490bb19e1a2d617c324d228a
pnik <67695706+Pniked@users.noreply.github.com>
2025-11-20 11:15:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3112,6 +3112,10 @@ var app = (function () {
         url: "https://soundcloud.com/user-169674116/track53",
         answer: "TH10 MoF - Native Faith (ネイティブフェイス)"
     },
+    {
+        url: "https://soundcloud.com/user-169674116/track35",
+        answer: "TH4 LLS - Bad Apple!!"
+    },
         ],
         Pn
       ).subscribe,
