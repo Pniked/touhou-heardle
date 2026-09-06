9e56ef82a0fea8bee12485d8a49ceffafdbeaa3e
pnik <67695706+Pniked@users.noreply.github.com>
2026-09-07 00:18:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4282,6 +4282,10 @@ var app = (function () {
         url: "https://soundcloud.com/user-169674116/track65",
         answer: "TH13.5 HM - Akutagawa Ryuunosuke's Kappa ~ Candid Friend (芥川龍之介の河童 ～ Candid Friend)"
     },
+    {
+        url: "https://soundcloud.com/user-169674116/track41",
+        answer: "TH14 DDC - Mist Lake (ミストレイク)"
+    },
         ],
         Pn
       ).subscribe,
