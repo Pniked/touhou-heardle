f9503da6c2fdb15170084f885f9b36020f506bfd
pnik <67695706+Pniked@users.noreply.github.com>
2026-09-19 00:12:55 +0700

    Update main.js

--- main.js
+++ main.js
@@ -4349,6 +4349,10 @@ var app = (function () {
         url: "https://soundcloud.com/user-76726180/touhou-15-lolk-faraway-380000-kilometer-voyage?in=yuli0/sets/all-touhou-ost",
         answer: "TH15 LoLK - Faraway Voyage of 380,000 Kilometers (遥か38万キロのボヤージュ)"
     },
+    {
+        url: "https://soundcloud.com/user-169674116/track43",
+        answer: "TH8 IN - Cinderella Cage ~ Kagome, Kagome (シンデレラケージ ～ Kagome-Kagome)"
+    },
         ],
         Pn
       ).subscribe,
